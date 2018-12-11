894f668a717d8c03702775d3abe82d85ac75a879
sohel-ahmed <sohelaiub2012@gmail.com>
2018-12-11 18:42:23 +0600

    rendering list WIP

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Counter extends Component{
 	state = {
 		count: 1,
-		imageUrl: 'https://picsum.photos/'
+		tags: ['tag1', 'tag2', 'tag3']
 	}
 
 	styles = {
@@ -24,6 +24,9 @@ class Counter extends Component{
 			<React.Fragment>
 				<span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button className="btn btn-secondary btn-sm">Increment</button>
+				<ul>
+					{this.state.tags.map(tag => <li>{ tag }</li>)}
+				</ul>
 			</React.Fragment>
 			);
 	}
