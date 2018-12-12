d1cf0908a1c61b7258dd7abec332fe6eb4a3a2f0
sohel-ahmed <sohelaiub2012@gmail.com>
2018-12-12 13:30:00 +0600

    handlening events with 2 approach

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,22 +3,42 @@ import React, { Component } from 'react';
 class Counter extends Component{
 	state = {
 		count: 1,
-		tags: []
+		tags: ['tag1', 'tag2', 'tag3']
 	}
 
-	renderTags(){
-		if(this.state.tags.length === 0) return <p>There are no tags</p>;
-
-		return <ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>
+	//for handling events if arrow function not work in future
+	// constructor(){
+	// 	super();
+	// 	this.handleIncrement = this.handleIncrement.bind(this);
+	// }
+	
+	
+	handleIncrement = () => {
+		console.log('Increment Clicked ', this);
 	}
 	render(){
 		return (
 			<React.Fragment>
-				{ this.state.tags.length === 0 && "Please create a new tags" }
-				{ this.renderTags() }
-				}
+				<span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+				<button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+				<ul>
+					{this.state.tags.map(tag => <li>{ tag }</li>)}
+				</ul>
 			</React.Fragment>
 			);
+	}
+	styles = {
+		fontSize: 20,
+		fontWeight: 'bold'
+	}
+	formatCount(){
+		const {count} = this.state;
+		return count === 0 ? 'zero' : count;
+	}
+	getBadgeClasses(){
+		let classes = "badge m-2 badge-";
+		classes += (this.state.count === 0 ? 'warning' : 'primary');
+		return classes;
 	}
 }
 
